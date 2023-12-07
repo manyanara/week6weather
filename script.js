23c6f88b73f6a4eca06a5496917f48507f3b930f
Manya <Manya@Khangals-MacBook-Air.local>
2023-12-06 22:31:29 -0800

    added a form input for country codes, attempted to test, fetch request is not using the https api url i added from the website

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 var cityInputEl = document.getElementById('cityName')
 var stateInputEl = document.getElementById('stateName')
+var countryInputEl= document.getElementById('countryName')
 
 //if check box delete state and add country code
 
 var formSubmitHandler = function(event){
     event.preventDefault();
 
-    location= cityInputEl.value.trim() + "," + stateInputEl
+    location= cityInputEl.value.trim() + "," + stateInputEl.value.trim() + "," + countryInputEl.value.trim()
 
     if(location){
         // start fetch for cityname.lon and cityname.lat
@@ -21,15 +22,13 @@ var formSubmitHandler = function(event){
 //fetch for city long and lat 
 function getLongLat(name){
     var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=" + name + "&limit={limit}&appid=1bd37a4c5340a8132a06dfe2bcb52a94"
-
+    console.log(apiUrl)
     fetch(apiUrl)
     .then(function (response) {
         if (response.ok) {
           response.json().then(function (data) {
-            lat = response.lat
-            lon = response.lon
-            getDailyForecast(data);
-            getWeeklyForecast (data)
+            getDailyForecast(data.lat, data.lon);
+            getWeeklyForecast (data.lat, data.lon)
           });
         } else {
           alert('Error: ');
@@ -37,15 +36,38 @@ function getLongLat(name){
       })
     }
 // fetch for weather forecase for long and lat input call function to display infor
-function getDailyForecast(town){
-    var apiUrl = 
+function getDailyForecast(lat, lon){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=1bd37a4c5340a8132a06dfe2bcb52a94"
+
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            //get temp humidity and wind 
+            console.log(data)
+            showDailyForecast(data.main.temp, data.main.humidity, data.wind.speed);
+        })
+    })
 }
 
-function getWeeklyForecast(town){
+function getWeeklyForecast(lat, lon){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=1bd37a4c5340a8132a06dfe2bcb52a94"
 
+    fetch(apiUrl).then(function(response){
+        response.json().then(function(data){
+            showWeeklyForecast(data);
+        })
+    })
 }
-
+// weekly forecast will require a forloop for 5 days
 // function to display forecast
+function showDailyForecast(temp, humidity, wind){
+    if (temp=== null){
+      //error message
+    }
+    
+    for(i=0; i<5; i++){
+
+    }
+}
 
 
-document.querySelector('button').addEventListener("click", formSubmitHandler());
+document.querySelector('button').addEventListener("click", formSubmitHandler);
